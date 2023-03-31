8cf0a7304b05fed1f399a3536f4e87e367d53f54
Riku Rauhala <riku@rauhala.io>
2023-03-31 09:26:02 +0300

    Add support for Redis

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,6 +1,4 @@
-const REDIS_URL = process.env.REDIS_URL || undefined
-
 module.exports = {
   MONGO_URL: 'mongodb://user:pass@localhost:3456/database',
-  REDIS_URL//: '//localhost:6378'
+  REDIS_URL: '//localhost:6378',
 }
